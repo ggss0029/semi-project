c55bb2cbe7dc4f8d4af3d2fb7e5c5c7a398137dc
ggss0029 <ggss0029@naver.com>
2023-04-25 10:49:15 +0900

    커미이이이잇

--- SemiProject/src/com/udong/board/news/controller/NewsBoardLikeCountController.java
+++ SemiProject/src/com/udong/board/news/controller/NewsBoardLikeCountController.java
@@ -1,26 +1,23 @@
 package com.udong.board.news.controller;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.udong.board.news.model.service.NewsBoardService;
-
 /**
- * Servlet implementation class NewBoardReplyUpdateController
+ * Servlet implementation class NewsBoardLikeCountController
  */
-@WebServlet("/newsUpdateReply.bo")
-public class NewBoardReplyUpdateController extends HttpServlet {
+@WebServlet("/newsLikeCount.do")
+public class NewsBoardLikeCountController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NewBoardReplyUpdateController() {
+    public NewsBoardLikeCountController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,22 +26,16 @@ public class NewBoardReplyUpdateController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int newsReplyNo = Integer.parseInt(request.getParameter("replyNo"));
-		String content = request.getParameter("content");
-		
-		int result = new NewsBoardService().newsUpdateReply(newsReplyNo, content);
-		
-		response.setContentType("json/application; charset=UTF-8");
-		response.getWriter().print(result);
-		
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
--- SemiProject/src/com/udong/board/news/model/service/NewsBoardService.java
+++ SemiProject/src/com/udong/board/news/model/service/NewsBoardService.java
@@ -115,4 +115,20 @@ public class NewsBoardService {
 		JDBCTemplate.close(conn);
 		return result;
 	}
+
+	public int newsUpdateReply(int newsReplyNo, String content) {
+		Connection conn = JDBCTemplate.getConnection();
+		
+		int result = new NewsBoardDao().newsUpdateReply(conn, newsReplyNo, content);
+		
+		if(result > 0) {
+			JDBCTemplate.commit(conn);
+		}else {
+			JDBCTemplate.rollback(conn);
+		}
+		
+		JDBCTemplate.close(conn);
+		
+		return result;
+	}
 }
