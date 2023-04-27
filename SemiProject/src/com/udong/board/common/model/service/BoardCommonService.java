7ab2e34dc8c360ee9f9d66ba83040446cf4b2e40
hansuung <hd9810@naver.com>
2023-04-27 14:58:33 +0900

    검색 결과 페이지 끝났어요 ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ

--- SemiProject/src/com/udong/board/common/model/service/BoardCommonService.java
+++ SemiProject/src/com/udong/board/common/model/service/BoardCommonService.java
@@ -20,18 +20,10 @@ public class BoardCommonService {
 		return list;
 	}
 
-	public int selectListCount() {
-
-		Connection conn = JDBCTemplate.getConnection();
-		int listCount = new BoardCommonDao().selectListCount(conn);
-		JDBCTemplate.close(conn);
-		return listCount;
-	}
-
-	public ArrayList<BoardCommon> searchKeyword(String keyword) {
+	public ArrayList<BoardCommon> searchKeyword(String keyword, PageInfo pi) {
 		
 		Connection conn = JDBCTemplate.getConnection();
-		ArrayList<BoardCommon> list = new BoardCommonDao().searchKeyword(conn,keyword);
+		ArrayList<BoardCommon> list = new BoardCommonDao().searchKeyword(conn,keyword,pi);
 		JDBCTemplate.close(conn);
 		return list;
 	}
@@ -67,6 +59,46 @@ public class BoardCommonService {
 		JDBCTemplate.close(conn);
 		return list;
 		
+	}
+
+	public int selectKeywordCount(String keyword) {
+
+		Connection conn = JDBCTemplate.getConnection();
+		int listCount = new BoardCommonDao().selectKeywordCount(conn,keyword);
+		JDBCTemplate.close(conn);
+		return listCount;
+	}
+
+	public int selectListCountTitleTime(int time, String searchInput) {
+		
+		Connection conn = JDBCTemplate.getConnection();
+		int listCount = new BoardCommonDao().selectListCountTitleTime(conn,time,searchInput);
+		JDBCTemplate.close(conn);
+		return listCount;
+	}
+
+	public int selectListCountContentTime(int time, String searchInput) {
+		
+		Connection conn = JDBCTemplate.getConnection();
+		int listCount = new BoardCommonDao().selectListCountContentTime(conn,time,searchInput);
+		JDBCTemplate.close(conn);
+		return listCount;
+	}
+
+	public int selectListCountReplyTime(int time, String searchInput) {
+
+		Connection conn = JDBCTemplate.getConnection();
+		int listCount = new BoardCommonDao().selectListCountReplyTime(conn,time,searchInput);
+		JDBCTemplate.close(conn);
+		return listCount;
+	}
+
+	public int selectListCountWriterTime(int time, String searchInput) {
+
+		Connection conn = JDBCTemplate.getConnection();
+		int listCount = new BoardCommonDao().selectListCountWriterTime(conn,time,searchInput);
+		JDBCTemplate.close(conn);
+		return listCount;
 	}
 
 }
